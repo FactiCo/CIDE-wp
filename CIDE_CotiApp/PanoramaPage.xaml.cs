085778aed2f0ac3e921c2cde58b2bd1b265e2021
MarioDevs <internetmajl@hotmail.com>
2015-01-12 17:17:18 -0600

    Checar AppID y llamadas asincronas close #19

--- CIDE_CotiApp/PanoramaPage.xaml.cs
+++ CIDE_CotiApp/PanoramaPage.xaml.cs
@@ -14,11 +14,21 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Windows.Controls.Primitives;
 using Microsoft.Phone.Tasks;
+using Facebook;
 
 namespace CIDE_CotiApp
 {
     public partial class PanoramaPage : PhoneApplicationPage
     {
+        
+        private const string AppId = "3350307419";
+        private const string ExtendedPermissions = "user_about_me,read_stream,publish_stream";
+
+        private readonly FacebookClient _fb = new FacebookClient();
+
+
+
+
         private Popup popup;
         public PanoramaPage()
         {
@@ -250,7 +260,82 @@ namespace CIDE_CotiApp
         private void lstScroll_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             //lstScroll.ScrollIntoView(lstScroll.SelectedIndex);
-       } 
+       }
+
+        private void btnFB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void webBrowserFB_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            FacebookOAuthResult oauthResult;
+            if (!_fb.TryParseOAuthCallbackUrl(e.Uri, out oauthResult))
+            {
+                return;
+            }
+
+            if (oauthResult.IsSuccess)
+            {
+                var accessToken = oauthResult.AccessToken;
+                LoginSucceded(accessToken);
+            }
+            else
+            {
+                // user cancelled
+                MessageBox.Show(oauthResult.ErrorDescription);
+            }
+
+        }
+
+        private void LoginSucceded(string accessToken)
+        {
+            var fb = new FacebookClient(accessToken);
+
+            fb.GetCompleted += (o, e) =>
+            {
+                if (e.Error != null)
+                {
+                    Dispatcher.BeginInvoke(() => MessageBox.Show(e.Error.Message));
+                    return;
+                }
+
+                var result = (IDictionary<string, object>)e.GetResultData();
+                var id = (string)result["id"];
+
+                var url = string.Format("/Pages/FacebookInfoPage.xaml?access_token={0}&id={1}", accessToken, id);
+
+                Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri(url, UriKind.Relative)));
+            };
+
+            fb.GetAsync("me?fields=id");
+        }
+
+        private void webBrowserFB_Loaded(object sender, RoutedEventArgs e)
+        {
+            var loginUrl = GetFacebookLoginUrl(AppId, ExtendedPermissions);
+
+            webBrowserFB.Navigate(loginUrl);
+
+        }
+
+        private Uri GetFacebookLoginUrl(string appId, string extendedPermissions)
+        {
+            var parameters = new Dictionary<string, object>();
+            parameters["client_id"] = appId;
+            parameters["redirect_uri"] = "https://www.facebook.com/connect/login_success.html";
+            parameters["response_type"] = "token";
+            parameters["display"] = "touch";
+
+            // add the 'scope' only if we have extendedPermissions.
+            if (!string.IsNullOrEmpty(extendedPermissions))
+            {
+                // A comma-delimited list of permissions
+                parameters["scope"] = extendedPermissions;
+            }
+
+            return _fb.GetLoginUrl(parameters);
+        }
 
     }
 }
